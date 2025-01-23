2d805a65c7f0152ffdc583fdfb98435ac9fb0181
mauyz <tsiorymauyz@gmail.com>
2025-01-23 22:18:25 +0300

    update default app

---
+++ lib/app/app.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class App extends StatelessWidget {
+
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: Scaffold(
+        body: Center(child: Text("Code de travail")),
+      ),
+    );
+  }
+}
