45f1d3153b5b20956e267a1df3be501137e15a21
mauyz <tsiorymauyz@gmail.com>
2025-01-24 23:41:44 +0300

    implement WorkCodeRepository

--- lib/data/repository/work_code_repository_impl.dart
+++ lib/data/repository/work_code_repository_impl.dart
@@ -1,14 +1,14 @@
-import 'package:madagascar_workcoode/data/datasource/json_parser.dart';
+import 'package:madagascar_workcoode/data/datasource/json_data_parser.dart';
 import 'package:madagascar_workcoode/domain/model/article.dart';
 import 'package:madagascar_workcoode/domain/model/work_code.dart';
 import 'package:madagascar_workcoode/domain/repository/work_code_repository.dart';
 
 class WorkCodeRepositoryImpl implements WorkCodeRepository {
-  final jsonParser = JsonParser.instance;
+  final jsonParser = JsonDataParser.instance;
 
   @override
   Future<WorkCode> getWWorkCode() {
-    return jsonParser.parseJson();
+    return jsonParser.parse();
   }
 
   @override
