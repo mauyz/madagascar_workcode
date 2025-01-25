d11165b1ccf877841684b9557c8829c132f719cd
mauyz <tsiorymauyz@gmail.com>
2025-01-25 22:04:41 +0300

    add allArticle getter

--- lib/domain/model/headline.dart
+++ lib/domain/model/headline.dart
@@ -1,5 +1,8 @@
 import 'package:madagascar_workcoode/domain/model/article.dart';
 import 'package:madagascar_workcoode/domain/model/chapter.dart';
+import 'package:madagascar_workcoode/domain/model/section.dart';
+import 'package:madagascar_workcoode/domain/model/subsection.dart';
+import 'package:madagascar_workcoode/domain/model/symbol_section.dart';
 
 class Headline {
   final String value;
@@ -39,5 +42,51 @@ class Headline {
       'chapters': chapters?.map((chapter) => chapter.toJson()).toList(),
       'articles': articles?.map((article) => article.toJson()).toList(),
     };
+  }
+
+  List<Article> get allArticles {
+    final articleList = <Article>[];
+    if (articles != null) {
+      articleList.addAll(articles!);
+    }
+    if (chapters != null) {
+      for (Chapter chapter in chapters!) {
+        if (chapter.articles != null) {
+          articleList.addAll(chapter.articles!);
+        }
+        final sections = chapter.sections;
+        if (sections != null) {
+          for (Section section in sections) {
+            if (section.articles != null) {
+              articleList.addAll(section.articles!);
+            }
+            final subsections = section.subsections;
+            if (subsections != null) {
+              for (Subsection subsection in subsections) {
+                if (subsection.articles != null) {
+                  articleList.addAll(subsection.articles!);
+                }
+                final symbols = subsection.symbols;
+                if (symbols != null) {
+                  for (SymbolSection symbol in symbols) {
+                    articleList.addAll(symbol.articles);
+                  }
+                }
+              }
+            }
+            final symbols = section.symbols;
+            if (symbols != null) {
+              for (SymbolSection symbol in symbols) {
+                articleList.addAll(symbol.articles);
+              }
+            }
+          }
+        }
+      }
+    }
+    articleList.sort(
+      (a, b) => a.number.compareTo(b.number),
+    );
+    return articleList;
   }
 }
