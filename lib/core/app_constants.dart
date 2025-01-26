6f64f24ec39e904a8818eb52542c437d207e42a6
mauyz <tsiorymauyz@gmail.com>
2025-01-26 12:19:36 +0300

    update app_constants.dart

--- lib/core/app_constants.dart
+++ lib/core/app_constants.dart
@@ -8,14 +8,14 @@ class AppConstants {
   static const appTitle = "Code du travail Malagasy";
   static const appVersion = "v1.0.0";
 
-  static const aboutApp = "Code du travail de Madagascar"
-      "L’Assemblée nationale et le Sénat ont adopté lors de leurs séances"
+  static const aboutApp = "Cette loi est"
+      " adoptée par l’Assemblée nationale et le Sénat lors de leurs séances"
       " plénières respectives en date du 20 juin 2024 et du 1er juillet 2024.\n"
       "Source : https://bit.ly/3CqSYRc";
   static const appClause =
       "Clause de non-responsabilité :\nCette application n'est pas affiliée à "
       "une entité gouvernementale et ne représente pas une autorité officielle.\n"
-      "Elle vise à offrir un accès simplifié au Code du trvavail Malagasy "
+      "Elle vise à offrir un accès simplifié au Code du travail "
       "pour des fins informatives et éducatives uniquement.";
 
   static final copyright = "© ${DateTime.now().year} Moïse T.";
