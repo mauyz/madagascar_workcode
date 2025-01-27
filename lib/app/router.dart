36ee52d70ef6dd50ac2407e2ed83f7a0689b3a5b
mauyz <tsiorymauyz@gmail.com>
2025-01-27 14:04:43 +0300

    fix app router

--- lib/app/router.dart
+++ lib/app/router.dart
@@ -1,5 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:go_router/go_router.dart';
 import 'package:madagascar_workcoode/presentation/article/article_content_page.dart';
+import 'package:madagascar_workcoode/presentation/headline/headline_page.dart';
 import 'package:madagascar_workcoode/presentation/headline/introduction_page.dart';
 import 'package:madagascar_workcoode/presentation/home/home_page.dart';
 import 'package:madagascar_workcoode/presentation/not_found_page.dart';
@@ -12,25 +14,40 @@ final router = GoRouter(
       builder: (context, state) => HomePage(),
       routes: [
         GoRoute(
-          path: "/articles/:id",
+          path: "articles/:id",
           builder: (context, state) {
-            final param = state.pathParameters["id"];
-            if (param != null) {
-              try {
-                final id = int.tryParse(param);
-                if (id != null && id > 0 && id < 396) {
-                  return ArticleContentPage(id: id);
-                }
-              } catch (_) {}
+            final id = int.tryParse(state.pathParameters["id"] ?? '');
+            if (id != null && id > 0 && id < 396) {
+              return ArticleContentPage(id: id);
             }
             return NotFoundPage();
           },
         ),
         GoRoute(
-          path: "/intro",
+          path: "intro",
+          builder: (context, state) => IntroductionPage(),
+        ),
+        GoRoute(
+          path: "headlines/:index",
           builder: (context, state) {
-            return IntroductionPage();
+            final index = int.tryParse(state.pathParameters["index"] ?? '');
+            if (index != null && index > 0 && index < 12) {
+              return HeadlinePage(index: index);
+            }
+            return NotFoundPage();
           },
+          routes: [
+            GoRoute(
+              path: "articles/:id",
+              builder: (context, state) {
+                final id = int.tryParse(state.pathParameters["id"] ?? '');
+                if (id != null && id > 0 && id < 396) {
+                  return ArticleContentPage(id: id);
+                }
+                return NotFoundPage();
+              },
+            ),
+          ],
         ),
       ],
     ),
