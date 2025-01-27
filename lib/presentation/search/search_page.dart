f8266378d802eb24887bd93df43d08be15c3ec77
mauyz <tsiorymauyz@gmail.com>
2025-01-27 15:38:19 +0300

    implement search_page.dart

---
+++ lib/presentation/search/search_page.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:madagascar_workcoode/presentation/search/bloc/search_bloc.dart';
+import 'package:madagascar_workcoode/presentation/search/bloc/search_state.dart';
+import 'package:madagascar_workcoode/presentation/search/widget/search_input_widget.dart';
+import 'package:madagascar_workcoode/presentation/search/widget/searched_article_list_view.dart';
+
+class SearchPage extends StatelessWidget {
+  const SearchPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider<SearchBloc>(
+      create: (context) => SearchBloc(),
+      child: Scaffold(
+        appBar: AppBar(
+          title: SearchInputWidget(),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: BlocBuilder<SearchBloc, SearchState>(
+            builder: (context, state) {
+              return switch (state) {
+                SearchInitial() => const SizedBox.shrink(),
+                SearchLoading() => Center(
+                    child: CircularProgressIndicator(),
+                  ),
+                SearchSuccess() => state.articles.isNotEmpty
+                    ? SearchedArticleListView(
+                        articles: state.articles,
+                        word: state.word,
+                      )
+                    : const Center(
+                        child: Text('Aucun résultat'),
+                      )
+              };
+            },
+          ),
+        ),
+      ),
+    );
+  }
+}
