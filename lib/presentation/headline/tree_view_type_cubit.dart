a46b1fb8b641fefb5ae5ef917b323bc810e9ab80
mauyz <tsiorymauyz@gmail.com>
2025-02-19 19:57:22 +0300

    implement tree_view_type_cubit.dart

---
+++ lib/presentation/headline/tree_view_type_cubit.dart
@@ -0,0 +1,16 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:madagascar_workcoode/core/service_locator.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class TreeViewTypeCubit extends Cubit<bool> {
+  
+  TreeViewTypeCubit(super.initialState);
+  
+  void toggle() {
+    final sharedPreferences = locator.get<SharedPreferences>();
+    final newViewType = !(sharedPreferences.getBool("viewType") ?? false);
+    sharedPreferences.setBool("viewType", newViewType);
+    emit(newViewType);
+  }
+  
+}
